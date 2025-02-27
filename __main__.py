3a5856b9895d2a64035fd190d9e23600cfac17cc
LucaNyckees <nyckees.luca@gmail.com>
2025-02-27 09:13:46 +0100

    logs and typer cmds

--- __main__.py
+++ __main__.py
@@ -1,16 +1,11 @@
+import typer
+
+from src.nlp import sentiment_analysis
 
 
+app = typer.Typer()
 
+app.add_typer(sentiment_analysis.app)
 
-if __name__ == '__main__':
-    print("hello")
-
-
-    # dataframes ->  llm 
-
-
-
-
-
-
-    
+if __name__ == "__main__":
+    app()
